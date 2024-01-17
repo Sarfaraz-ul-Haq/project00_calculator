28f237846beab2f1a7556c7fa9f09767b34085f3
Sarfaraz <isarfaraz80@gmail.com>
2024-01-17 18:36:50 +0500

    uncommented show CalcTitleAndImage function

--- app.ts
+++ app.ts
@@ -4,31 +4,31 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import showBanner from "node-banner";
 
-// //function to display calculator title and image
-// const showCalcTitleAndImage = () => {
-//   showBanner(
-//     "Calculator",
-//     `Simple Command Line Calculator written in TypeScript & Node.js
-//     _____________________
-//     |  _________________  |
-//     | |            0. | |
-//     | |_________________| |
-//     |  ___ ___ ___   ___  |
-//     | | 7 | 8 | 9 | | + | |
-//     | |___|___|___| |___| |
-//     | | 4 | 5 | 6 | | - | |
-//     | |___|___|___| |___| |
-//     | | 1 | 2 | 3 | | x | |
-//     | |___|___|___| |___| |
-//     | | . | 0 | = | | / | |
-//     | |___|___|___| |___| |
-//     |_____________________|
-//     `,
-//     "green",
-//     "gray"
-//   );
-// };
-// showCalcTitleAndImage();
+//function to display calculator title and image
+const showCalcTitleAndImage = () => {
+  showBanner(
+    "Calculator",
+    `Simple Command Line Calculator written in TypeScript & Node.js
+    _____________________
+    |  _________________  |
+    | |            0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|
+    `,
+    "green",
+    "gray"
+  );
+};
+showCalcTitleAndImage();
 
 //calculator function
 const calculator = async() => {
